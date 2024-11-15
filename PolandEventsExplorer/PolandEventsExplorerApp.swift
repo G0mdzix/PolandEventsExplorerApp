0b6ea622b3ac80a8988c0577a75eb2ba7755561f
Mateusz Gozdzik <mateusz@gozdzik.xyz>
2024-11-15 19:41:11 +0100

    Initial Commit

---
+++ PolandEventsExplorer/PolandEventsExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolandEventsExplorerApp.swift
+//  PolandEventsExplorer
+//
+//  Created by Mateusz Gozdzik on 15/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct PolandEventsExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
